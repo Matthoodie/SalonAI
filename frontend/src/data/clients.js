28c02407dfaef57bd124e8023f3c7691c3695013
Matej Hudoletnjak <matej.hudoletnjak@gmail.com>
2026-07-27 23:58:54 +0200

    Move client data into separate data layer

---
+++ frontend/src/data/clients.js
@@ -0,0 +1,26 @@
+export const clients = [
+  {
+    id: 1,
+    name: 'Ana Horvat',
+    phone: '091 123 4567',
+    visits: 12,
+  },
+  {
+    id: 2,
+    name: 'Marko Marić',
+    phone: '098 555 111',
+    visits: 8,
+  },
+  {
+    id: 3,
+    name: 'Petra Novak',
+    phone: '099 777 888',
+    visits: 21,
+  },
+  {
+    id: 4,
+    name: 'Ivan Kovač',
+    phone: '095 444 222',
+    visits: 5,
+  },
+]
